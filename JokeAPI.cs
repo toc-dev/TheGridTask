04f3aa851934065f63c9493eefd6f632e8566e85
Tochukwu <tochukwusage4@gmail.com>
2023-12-12 15:25:51 +0100

    Add project files.

---
+++ JokeAPI.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGridTask.Console.Dtos;
+
+namespace TheGridTask.Console
+{
+    public class JokeAPIClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _jokeUrl;
+        public JokeAPIClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _jokeUrl = "https://sv443.net/jokeapi/v2/joke/Any";
+        }
+
+        public JokeDto GetJoke()
+        {
+            JokeDto joke = new();
+            var response = _httpClient.GetAsync(_jokeUrl).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                joke = JsonConvert.DeserializeObject<JokeDto>(response.Content.ReadAsStringAsync().Result);
+            }
+
+            return joke;
+        }
+    }
+}
---
+++ Models/JokeContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGridTask.Console.Models
+{
+    public class JokeContext : DbContext
+    {
+        public JokeContext(DbContextOptions<JokeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Joke> Jokes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
