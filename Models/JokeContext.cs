2daa6513187aa92623962d55c3237a98c5a454ac
Tochukwu <tochukwusage4@gmail.com>
2023-12-12 23:01:21 +0100

    made migrations to the database

--- Models/JokeContext.cs
+++ Models/JokeContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,10 @@ namespace TheGridTask.Console.Models
 {
     public class JokeContext : DbContext
     {
+        public JokeContext():base()
+        {
+
+        }
         public JokeContext(DbContextOptions<JokeContext> options) : base(options)
         {
 
@@ -16,7 +21,11 @@ namespace TheGridTask.Console.Models
         public DbSet<Joke> Jokes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
+            //var connectionString = ConfigurationManager.ConnectionStrings["GridJokesDB"].ConnectionString;
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder.UseSqlServer(connectionString);
+            //}
         }
     }
 }
